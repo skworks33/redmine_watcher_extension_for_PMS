0ff7ecbb6c086d01244a46916566b7fb1bd24ec5
skworks33 <skworks33@gmail.com>
2020-12-28 17:37:38 +0900

    Exclude project members from watchers

--- init.rb
+++ init.rb
@@ -2,11 +2,11 @@ require 'redmine'
 
 require_dependency 'issues_helper_patch_for_watchers'
 
-Redmine::Plugin.register :redmine_watcher_extension do
-  name 'Redmine Watcher Extension'
+Redmine::Plugin.register :redmine_watcher_extension_for_PMS do
+  name 'Redmine Watcher Extension for PMS'
   author 'skworks33'
-  description 'Redmine Watcher Extension'
+  description 'Redmine Watcher Extension for PMS'
   version '0.0.1'
-  url 'https://github.com/skworks33/redmine_watcher_extension'
+  url 'https://github.com/skworks33/redmine_watcher_extension_for_PMS'
   author_url 'https://github.com/skworks33'
 end
--- lib/issues_helper_patch_for_watchers.rb
+++ lib/issues_helper_patch_for_watchers.rb
@@ -5,9 +5,9 @@ module IssuesHelperPatch
   # Returns an array of users that are proposed as watchers
   # on the new issue form
   def users_for_new_issue_watchers(issue)
-    # Get project users
     users = issue.watcher_users.select{|u| u.status == User::STATUS_ACTIVE}
-    users = (users + issue.project.users.sort).uniq
+    # "PMS" does not need to get the project user.
+    # users = (users + issue.project.users.sort).uniq
 
     # Get the users of the group to which the user belongs
     User.current.groups.find_each do |g|
